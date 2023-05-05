624723ee6758245b1dcbed4327db3726cbb34634
SonicCoding1180 <18603248+SylvKT@users.noreply.github.com>
2023-05-05 18:22:59 +0000

    try this

--- src/main.rs
+++ src/main.rs
@@ -5,47 +5,46 @@ use std::fs::write;
 use std::future::Future;
 use std::process::exit;
 
-fn main() -> impl Future<Output=()> {
-	async {
-		let github_output_path = env::var("GITHUB_OUTPUT").expect("run this in a github action, you goober");
-		
-		let args: Vec<String> = env::args().collect();
-		let error = &args.get(1);
-		
-		if !error.is_none() {
-			let error = error.unwrap();
-			eprintln!("Error: {error}");
-			write(github_output_path, format!("error={error}")).unwrap();
-			exit(1);
-		}
-		
-		let client = reqwest::Client::builder()
-			.user_agent("SylvKT@github.com/modid-db-sylv (sylv.gay)") // gotta make sure they know it's me, else they'll block the connection if we spam too much.
-			.build().unwrap();
-		
-		// Request newest projects
-		let res = client.get("https://api.modrinth.com/v2/search")
-			.body(r#"\
-		{
-			"query": "",
-			"index": "newest",
-			"offset": 0,
-			"limit": 1,
-			"filters": "categories=quilt AND categories=fabric"
-		}\
-		"#) // hello modrinth employees! sorry for spamming, just doing some routine GDPR breaches :3
-			.send()
-			.await
-			.expect("Failed to request newest projects from Modrinth");
-		let text = res
-			.text()
-			.await
-			.expect("Failed to parse text from request");
-		
-		println!("body: {}", text);
-		
-		// Request each project's latest .jar
-		
-		// Unzip the .jar and retrieve the mod ID from the fabric.mod.json or quilt.mod.json
+#[tokio::main]
+async fn main() {
+	let github_output_path = env::var("GITHUB_OUTPUT").expect("run this in a github action, you goober");
+	
+	let args: Vec<String> = env::args().collect();
+	let error = &args.get(1);
+	
+	if !error.is_none() {
+		let error = error.unwrap();
+		eprintln!("Error: {error}");
+		write(github_output_path, format!("error={error}")).unwrap();
+		exit(1);
 	}
+	
+	let client = reqwest::Client::builder()
+		.user_agent("SylvKT@github.com/modid-db-sylv (sylv.gay)") // gotta make sure they know it's me, else they'll block the connection if we spam too much.
+		.build().unwrap();
+	
+	// Request newest projects
+	let res = client.get("https://api.modrinth.com/v2/search")
+		.body(r#"\
+	{
+		"query": "",
+		"index": "newest",
+		"offset": 0,
+		"limit": 1,
+		"filters": "categories=quilt AND categories=fabric"
+	}\
+	"#) // hello modrinth employees! sorry for spamming, just doing some routine GDPR breaches :3
+		.send()
+		.await
+		.expect("Failed to request newest projects from Modrinth");
+	let text = res
+		.text()
+		.await
+		.expect("Failed to parse text from request");
+	
+	println!("body: {}", text);
+	
+	// Request each project's latest .jar
+	
+	// Unzip the .jar and retrieve the mod ID from the fabric.mod.json or quilt.mod.json
 }
--- src/test.rs
+++ src/test.rs
@@ -2,5 +2,7 @@ use crate::main;
 
 #[tokio::test]
 async fn test_main() {
-	main().await // make the fucking thing work with cargo test
+	async {
+		main() // make the fucking thing work with cargo test
+	}.await
 }
