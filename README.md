e802743a925a0a201c2bac5d5db69374fad0194d
SylvKT <18603248+SylvKT@users.noreply.github.com>
2023-05-05 00:36:12 -0400

    the r-slur

--- README.md
+++ README.md
@@ -2,9 +2,26 @@
 
 Searches Modrinth and other places for mod IDs and adds them to the mod ID database. This can be used to check if a mod ID has already been taken by another mod.
 
+### Supported Mod Platforms
+- [x] Modrinth
+- [ ] CurseForge
+    - Since CurseForge's API is closed, we cannot add support for it.
+- [ ] <sub>~~Game Banana~~</sub>
+
+### Supported Mod Loaders
+- Fabric
+- Quilt
+- ~~Forge~~ (This may be added in the future, but not now)
+
+### How do I upload my own mod ID?
+Currently, you cannot. However, this a planned feature.
+
+### MSRV <sub>why do you need an MSRV on a g~~ithub actio~~</sub>
+We support Rust 1.69 because it is a pretty nice version.
+
 ## TODO
 
 - [ ] Update this README with the details of this action
 - [ ] Update inputs/outputs in `action.yaml`
 - [ ] Implement the action's logic in `src/main.rs`
-- [ ] Rename the default Git branch to `v1` (instead of `main` or `master`. This helps with potential future breaking changes. **PROVIDED ACTIONS WILL NOT WORK UNTIL YOU DO THIS** 
+- [ ] Rename the default Git branch to `v1` (instead of `main` or `master`.) This helps with potential future breaking changes. **PROVIDED ACTIONS WILL NOT WORK UNTIL YOU DO THIS** 
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,45 @@ use std::env;
 use std::fs::write;
 use std::process::exit;
 
-fn main() {
-    let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
-
-    let args: Vec<String> = env::args().collect();
-    let error = &args[1];
-
-    if !error.is_empty() {
-        eprintln!("Error: {error}");
-        write(github_output_path, format!("error={error}")).unwrap();
-        exit(1);
-    }
+#[tokio::main]
+async fn main() {
+	let github_output_path = env::var("GITHUB_OUTPUT").expect("run this in a github action, you goober");
+	
+	let args: Vec<String> = env::args().collect();
+	let error = &args[1];
+	
+	let client = reqwest::Client::builder()
+		.user_agent("SylvKT@github.com/modid-db-sylv (sylv.gay)") // gotta make sure they know it's me, else they'll block the connection if we spam too much.
+		.build().unwrap();
+	
+	// Request newest projects
+	let res = client.get("https://api.modrinth.com/v2/search")
+		.body(r#"\
+		{
+			"query": "",
+			"index": "newest",
+			"offset": 0,
+			"limit": 1,
+			"filters": "categories=quilt AND categories=fabric"
+		}\
+		"#) // hello modrinth employees! sorry for spamming, just doing some routine GDPR breaches :3
+		.send()
+		.await
+		.expect("Failed to request newest projects from Modrinth");
+	let text = res
+		.text()
+		.await
+		.expect("Failed to parse text from request");
+	
+	println!("body: {}", text);
+	
+	// Request each project's latest .jar
+	
+	// Unzip the .jar and retrieve the mod ID from the fabric.mod.json or quilt.mod.json
+	
+	if !error.is_empty() {
+		eprintln!("Error: {error}");
+		write(github_output_path, format!("error={error}")).unwrap();
+		exit(1);
+	}
 }
