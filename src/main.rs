b3bd440866d6cafbad69a14c0142992c8c40a33c
SylvKT <18603248+SylvKT@users.noreply.github.com>
2023-05-05 01:56:45 -0400

    fix error little fucker

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,14 @@ async fn main() {
 	let github_output_path = env::var("GITHUB_OUTPUT").expect("run this in a github action, you goober");
 	
 	let args: Vec<String> = env::args().collect();
-	let error = &args[1];
+	let error = &args.get(1);
+	
+	if !error.is_none() {
+		let error = error.unwrap();
+		eprintln!("Error: {error}");
+		write(github_output_path, format!("error={error}")).unwrap();
+		exit(1);
+	}
 	
 	let client = reqwest::Client::builder()
 		.user_agent("SylvKT@github.com/modid-db-sylv (sylv.gay)") // gotta make sure they know it's me, else they'll block the connection if we spam too much.
@@ -39,10 +46,4 @@ async fn main() {
 	// Request each project's latest .jar
 	
 	// Unzip the .jar and retrieve the mod ID from the fabric.mod.json or quilt.mod.json
-	
-	if !error.is_empty() {
-		eprintln!("Error: {error}");
-		write(github_output_path, format!("error={error}")).unwrap();
-		exit(1);
-	}
 }
