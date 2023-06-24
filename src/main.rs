30995028e2b102119b0862fac9c51d4643f2e2de
SylvKT <contact@sylv.gay>
2023-06-24 00:46:12 -0400

    redirect from http to https

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::io::BufReader;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
 use std::time::Duration;
 use actix_web::{App, get, HttpResponse, HttpServer, web};
+use actix_web_middleware_redirect_scheme::RedirectSchemeBuilder;
 use ferinth::Ferinth;
 use rustls_pemfile::{certs, pkcs8_private_keys};
 use serde::{Deserialize, Serialize};
@@ -50,6 +51,7 @@ async fn main() {
 	// Start actix server
 	let server = HttpServer::new(move || {
 		App::new()
+			.wrap(RedirectSchemeBuilder::new().build())
 			.app_data(web::Data::new(pool_ref.clone()))
 			.app_data(web::Data::new(fer.clone()))
 			.service(default)
@@ -61,9 +63,7 @@ async fn main() {
 			.expect("Failed to load certificates");
 		
 		server
-			.bind_rustls(SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 80), certs.clone())
-			.expect("Failed to bind to address 0.0.0.0 on port 80")
-			.bind_rustls(SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 443), certs.clone())
+			.bind_rustls(SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 443), certs)
 			.expect("Failed to bind to address 0.0.0.0 on port 443")
 			.run()
 			.await
